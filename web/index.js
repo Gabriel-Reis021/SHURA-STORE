c2b25993a8ee6af44554be492961f54020abd547
Gabriel-Reis021 <gabrielreissantos180@gmail.com>
2025-08-11 22:24:01 -0300

    feat: adicionando novos estilos ao carrosel e links de contato no footer

--- web/index.js
+++ web/index.js
@@ -3,13 +3,35 @@ let count = 1;
 document.getElementById("radio1").checked = true;
 
 setInterval(() => {
-    nextImage();
+  nextImage();
 }, 5000);
 
-function nextImage(){
-    count++;
-    if(count>4){
-        count = 1;
-    }
-    document.getElementById("radio" + count).checked = true;
+function nextImage() {
+  count++;
+  if (count > 4) {
+    count = 1;
+  }
+  document.getElementById("radio" + count).checked = true;
 }
+
+const icones = document.querySelectorAll(".icones");
+
+icones.forEach((icone) => {
+  icone.addEventListener("click", () => {
+    const nome = icone.getAttribute("name");
+    let url = "";
+    if (nome === "logo-linkedin") {
+      url = "https://www.linkedin.com/in/gabriel-reis-9aab63239/";
+      window.open(url, "_blank");
+    } else if (nome === "logo-github") {
+      url = "https://github.com/Gabriel-Reis021";
+      window.open(url, "_blank");
+    } else if (nome === "logo-instagram") {
+      url = "https://www.instagram.com/gabrielsantzs_/";
+      window.open(url, "_blank");
+    } else if (nome === "logo-whatsapp") {
+      url = "https://wa.me/21974214951";
+      window.open(url, "_blank");
+    }
+  });
+});
